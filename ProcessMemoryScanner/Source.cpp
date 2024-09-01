ecb92e16a5562284e3e464ee046f92f1987fe890
artur <muslimow9@yandex.ru>
2024-09-01 17:30:55 +0300

    Stable version. Added driver-mode

--- ProcessMemoryScanner/Source.cpp
+++ ProcessMemoryScanner/Source.cpp
@@ -1,7 +1,7 @@
-#include <windows.h>
+#include "SharedStuffLib.h"
+#include "MemoryHackerLib.h"
 #include <easylogging++.h>
 #include <tclap/CmdLine.h>
-#include "SharedStuffLib.h"
 
 #define PRODUCT_NAME L"ProcessMemoryScanner"
 #define FILE_NAME "addresses.txt"
@@ -10,6 +10,37 @@
 #define USER_MODE_VM_MAX_ADDRESS (SIZE_T)0x7FFF'FFFFFFFF
 
 INITIALIZE_EASYLOGGINGPP
+
+/*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: SearchProcessAddresses()
+
+  Summary:  Searches for process addresses what point to specified value.
+            Found addresses will be written to the file.
+
+  Args:     _In_ HANDLE process,
+                The process where addresses are pointing.
+            _In_ DWORD64 value,
+                Value that addresses should point.
+            _In_ DWORD valueSize)
+                Value size.
+            _In_ BOOL isDriverMode
+                Specify if function should use kernel driver.
+
+  Returns:  void
+-----------------------------------------------------------------F-F*/
+void SearchProcessAddresses(
+    _In_ HANDLE process,
+    _In_ DWORD64 value,
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode);
+
+void SearchProcessAddressesRange(
+    _In_ HANDLE process,
+    _In_ MEMORY_BASIC_INFORMATION memoryInfo,
+    _In_ DWORD64 value,
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode,
+    _In_ FILE* pFile);
 
 /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   Function: FilterProcessAddresses()
@@ -24,13 +55,16 @@ INITIALIZE_EASYLOGGINGPP
                 Value to compare.
             _In_ DWORD valueSize)
                 Value size.
+            _In_ BOOL isDriverMode
+                Specify if function should use kernel driver.
 
   Returns:  void
 -----------------------------------------------------------------F-F*/
 void FilterProcessAddresses(
     _In_ HANDLE process,
     _In_ DWORD64 value,
-    _In_ DWORD valueSize);
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode);
 
 /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   Function: WriteProcessAddresses()
@@ -44,33 +78,25 @@ void FilterProcessAddresses(
                 Value to write.
             _In_ DWORD valueSize)
                 Value size.
+            _In_ BOOL isDriverMode
+                Specify if function should use kernel driver.
 
   Returns:  void
 -----------------------------------------------------------------F-F*/
 void WriteProcessAddresses(
     _In_ HANDLE process,
     _In_ DWORD64 value,
-    _In_ DWORD valueSize);
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode);
 
-/*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  Function: SearchProcessAddresses()
-
-  Summary:  Searches for process addresses what point to specified value.
-            Found addresses will be written to the file.
-
-  Args:     _In_ HANDLE process,
-                The process where addresses are pointing.
-            _In_ DWORD64 value,
-                Value that addresses should point.
-            _In_ DWORD valueSize)
-                Value size.
-
-  Returns:  void
------------------------------------------------------------------F-F*/
-void SearchProcessAddresses(
-    _In_ HANDLE process,
-    _In_ DWORD64 value,
-    _In_ DWORD valueSize);
+void ParseArguments(
+    _In_ int argc,
+    _In_ char** argv,
+    _Out_ std::string& command,
+    _Out_ std::string& valueStr,
+    _Out_ std::string& valueType,
+    _Out_ PDWORD pProcessId,
+    _Out_ PBOOL pIsDriverMode);
 
 DWORD64 ParseValue(
     _In_ std::string valueStr,
@@ -78,6 +104,7 @@ DWORD64 ParseValue(
     _In_ PDWORD pValueSize);
 
 void OutputFile();
+
 
 /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   Summary:  It initializes the logger, 
@@ -97,6 +124,7 @@ void OutputFile();
                         long 
                         float 
                         double
+                    --driver-mode
 
   Returns:  0
                 If success.
@@ -107,6 +135,13 @@ int main(int argc, char* argv[])
 {
     DWORD result = 0;
     HANDLE hProcess = NULL;
+    std::string command;
+    std::string valueStr;
+    std::string valueType;
+    DWORD processId = 0;
+    BOOL isDriverMode = 0;
+    DWORD valueSize = 0;
+    DWORD64 value = 0;  // DWORD64 can hold value with each type
 
     try {
 #ifdef _DEBUG
@@ -115,39 +150,15 @@ int main(int argc, char* argv[])
         ConfigureLoggers(PRODUCT_NAME, false);
 #endif
 
-        TCLAP::CmdLine cmd(
-            "`ProcessMemoryScanner` scans specified process for specified value.",
-            ' ',
-            "1.0");
-        TCLAP::UnlabeledValueArg<std::string> commandArg(
-            "command", "The following commands are available: search | filter | write", TRUE,
-            "search | filter | write",
-            "command", cmd);
-        TCLAP::UnlabeledValueArg<std::string> valueStrArg(
-            "value", "The value of one of the specified type", TRUE,
-            "500",
-            "value", cmd );
-        TCLAP::ValueArg<DWORD> processIdArg(
-            "", "process-id", "", TRUE,
-            1224,
-            "id", cmd );
-        TCLAP::ValueArg<std::string> valueTypeArg(
-            "", "value-type", "The following types are avaiable: int32 | int64 | float | double", TRUE,
-            "int32",
-            "int32 | int64 | float | double", cmd);
-        cmd.parse(argc, argv);
+        ParseArguments(argc, argv, command, valueStr, valueType, &processId, &isDriverMode);
+        value = ParseValue(valueStr, valueType, &valueSize);
 
-        std::string command = commandArg.getValue();
-        DWORD processId = processIdArg.getValue();
-        std::string valueType = valueTypeArg.getValue();
-        DWORD valueSize = 0;
-        // DWORD64 can hold value with each type
-        DWORD64 value = ParseValue(valueStrArg.getValue(), valueType, &valueSize);
         LOG(INFO) << "command: " << command;
         LOG(INFO) << "value as DWORD64: " << value;
         LOG(INFO) << "processIdArg: " << processId;
         LOG(INFO) << "valueType: " << valueType;
         LOG(INFO) << "valueSize: " << valueSize;
+        LOG(INFO) << "isDriverMode: " << isDriverMode;
 
         hProcess = OpenProcess(
             PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION,
@@ -156,20 +167,21 @@ int main(int argc, char* argv[])
         if (hProcess == NULL) {
             throw std::runtime_error("Failed to open process");
         }
-        LOG(INFO) << "hProcess: " << hProcess;
-
+        if (isDriverMode && IsDriverAvailable() == FALSE) {
+            throw std::runtime_error("The driver is not available");
+        }
 
         if (command == "search") {
-            SearchProcessAddresses(hProcess, value, valueSize);
+            SearchProcessAddresses(hProcess, value, valueSize, isDriverMode);
         }
         else if (command == "filter") {
-            FilterProcessAddresses(hProcess, value, valueSize);
+            FilterProcessAddresses(hProcess, value, valueSize, isDriverMode);
         }
         else if (command == "write") {
-            WriteProcessAddresses(hProcess, value, valueSize);
+            WriteProcessAddresses(hProcess, value, valueSize, isDriverMode);
         }
         else {
-            throw TCLAP::ArgException("Invalid command");
+            throw TCLAP::ArgException("Invalid command", "command");
         }
 
         OutputFile();
@@ -186,6 +198,77 @@ int main(int argc, char* argv[])
         CloseHandle(hProcess);
 
     return 0;
+}
+
+void ParseArguments(
+    _In_ int argc,
+    _In_ char** argv,
+    _Out_ std::string& command,
+    _Out_ std::string& valueStr,
+    _Out_ std::string& valueType,
+    _Out_ PDWORD pProcessId,
+    _Out_ PBOOL pIsDriverMode)
+{
+    TCLAP::CmdLine cmd(
+        "`ProcessMemoryScanner` scans specified process for specified value.",
+        ' ',
+        "1.0");
+    TCLAP::UnlabeledValueArg<std::string> commandArg(
+        "command", "The following commands are available: search | filter | write", TRUE,
+        "search | filter | write",
+        "command", cmd);
+    TCLAP::UnlabeledValueArg<std::string> valueStrArg(
+        "value", "The value of one of the specified type", TRUE,
+        "500",
+        "value", cmd );
+    TCLAP::ValueArg<DWORD> processIdArg(
+        "", "process-id", "", TRUE,
+        1224,
+        "id", cmd );
+    TCLAP::ValueArg<std::string> valueTypeArg(
+        "", "value-type", "The following types are avaiable: int32 | int64 | float | double", TRUE,
+        "int32",
+        "int32 | int64 | float | double", cmd);
+    TCLAP::SwitchArg isDriverModeSwitch(
+        "", "driver-mode", "Switch to use kernel driver to access specified process",
+        cmd, FALSE);
+    cmd.parse(argc, argv);
+
+    command = commandArg.getValue();
+    valueStr = valueStrArg.getValue();
+    valueType = valueTypeArg.getValue();
+    *pProcessId = processIdArg.getValue();
+    *pIsDriverMode = isDriverModeSwitch.getValue();
+}
+
+DWORD64 ParseValue(
+    _In_ std::string valueStr,
+    _In_ std::string valueType,
+    _In_ PDWORD pValueSize)
+{
+    DWORD64 value = 0;
+    if (valueType == "int32") {
+        value = (DWORD64)std::stol(valueStr);
+        *pValueSize = sizeof(INT32);
+    }
+    else if (valueType == "int64") {
+        value = (DWORD64)std::stoll(valueStr);
+        *pValueSize = sizeof(INT64);
+    }
+    else if (valueType == "float") {
+        FLOAT valueFloat = std::stof(valueStr);
+        memcpy(&value, &valueFloat, sizeof(FLOAT));
+        *pValueSize = sizeof(FLOAT);
+    }
+    else if (valueType == "double") {
+        DOUBLE valueDouble = std::stod(valueStr);
+        memcpy(&value, &valueDouble, sizeof(DOUBLE));
+        *pValueSize = sizeof(DOUBLE);
+    }
+    else {
+        throw TCLAP::ArgParseException("--value-type");
+    }
+    return value;
 }
 
 /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -206,17 +289,9 @@ int main(int argc, char* argv[])
 void SearchProcessAddresses(
     _In_ HANDLE process,
     _In_ DWORD64 value,
-    _In_ DWORD valueSize)
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode)
 {
-    // `buff` contains read memory
-    PBYTE buff = (PBYTE)VirtualAlloc(
-        NULL,
-        100 * BUFF_SIZE,
-        MEM_COMMIT | MEM_RESERVE,
-        PAGE_READWRITE);
-    if (buff == NULL) {
-        throw std::runtime_error("Not enough memory");
-    }
     MEMORY_BASIC_INFORMATION memoryInfo = { 0 };
     SIZE_T i = 0x0;
     FILE* pFile;
@@ -227,10 +302,10 @@ void SearchProcessAddresses(
 
     while (i < USER_MODE_VM_MAX_ADDRESS) {
         if (VirtualQueryEx(
-            process,
-            (LPCVOID)i,
-            &memoryInfo,
-            sizeof(memoryInfo)) == 0) {
+                process,
+                (LPCVOID)i,
+                &memoryInfo,
+                sizeof(memoryInfo)) == 0) {
             break;
         }
         if (memoryInfo.State == MEM_COMMIT) {
@@ -238,36 +313,72 @@ void SearchProcessAddresses(
                 LOG(WARNING) << "Found pages range with too big size: " << memoryInfo.RegionSize << ". Skipping it";
             }
             else {
-                LOG(INFO) << "Start of range:" << memoryInfo.BaseAddress;
-                LOG(INFO) << "End of range:" << (SIZE_T)memoryInfo.BaseAddress + memoryInfo.RegionSize;
-                LOG(INFO) << "Size of range:" << memoryInfo.BaseAddress;
-                if (ReadProcessMemory(
+                SearchProcessAddressesRange(
                     process,
-                    (LPCVOID)i,
-                    buff,
-                    memoryInfo.RegionSize,
-                    NULL)) {
-                    LOG_N_TIMES(5, INFO) << "buff contains (as DWORD64): " << *(PDWORD64)buff;
-                    for (SIZE_T buff_i = 0x0; buff_i < memoryInfo.RegionSize; buff_i += valueSize) {
-                        LOG_IF(buff_i >= BUFF_SIZE, FATAL) << "Buff length is not enough";
-                        if (memcmp(buff + buff_i, &value, valueSize) == 0) {
-                            LOG(INFO) << "Found: " << std::hex << i + buff_i;
-                            fprintf(pFile, "%p\n", (PVOID)(i + buff_i));
-                        }
-                    }
-                }
-                else {
-                    // ReadProcessMemory() could return 0 if it cannot open process or
-                    // it cannot read specified virtual memory range.
-                }
+                    memoryInfo,
+                    value,
+                    valueSize,
+                    isDriverMode,
+                    pFile);
             }
         }
         i += memoryInfo.RegionSize;
     }
-    VirtualFree(buff, 0, MEM_RELEASE);
 
     if (pFile)
         fclose(pFile);
+}
+
+void SearchProcessAddressesRange(
+    _In_ HANDLE process,
+    _In_ MEMORY_BASIC_INFORMATION memoryInfo,
+    _In_ DWORD64 value,
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode,
+    _In_ FILE* pFile)
+{
+    PBYTE buff = (PBYTE)VirtualAlloc(  // buff will contain read memory
+        NULL,
+        100 * BUFF_SIZE,
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_READWRITE);
+    BOOL result = FALSE;
+    if (buff == NULL) {
+        throw std::runtime_error("Not enough memory");
+    }
+
+    LOG(INFO) << "Start of range:" << memoryInfo.BaseAddress;
+    LOG(INFO) << "End of range:" << std::dec << (SIZE_T)memoryInfo.BaseAddress + memoryInfo.RegionSize;
+    LOG(INFO) << "Size of range:" << memoryInfo.BaseAddress;
+
+    if (isDriverMode == FALSE) {
+        result = ReadProcessMemory(
+            process,
+            (LPCVOID)memoryInfo.BaseAddress,
+            buff,
+            memoryInfo.RegionSize,
+            NULL);
+        // ReadProcessMemory() could return 0 if it cannot open process or
+        // it cannot read specified virtual memory range.
+    }
+    else {
+        result = ReadProcessMemoryDrivered(
+            process,
+            (LPCVOID)memoryInfo.BaseAddress,
+            buff,
+            memoryInfo.RegionSize,
+            NULL);
+    }
+    LOG(INFO) << (result ? "Reading is successed" : "Reading is failed");
+    if (result) {
+        for (SIZE_T buff_i = 0x0; buff_i < memoryInfo.RegionSize; buff_i += valueSize) {
+            LOG_IF(buff_i >= BUFF_SIZE, FATAL) << "Buff length is not enough";
+            if (memcmp(buff + buff_i, &value, valueSize) == 0) {
+                fprintf(pFile, "%p\n", (PVOID)((SIZE_T)memoryInfo.BaseAddress + buff_i));
+            }
+        }
+    }
+    VirtualFree(buff, 0, MEM_RELEASE);
 }
 
 /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -278,41 +389,55 @@ void SearchProcessAddresses(
             that still match the value will be written within the same file.
 
   Args:     _In_ HANDLE process,
-                The process where addresses are pointing.
+                The process where addresses are point.
             _In_ DWORD64 value,
                 Value to compare.
             _In_ DWORD valueSize)
                 Value size.
+            _In_ BOOL isDriverMode
+                Specify if function should use kernel driver.
 
   Returns:  void
 -----------------------------------------------------------------F-F*/
 void FilterProcessAddresses(
     _In_ HANDLE process,
     _In_ DWORD64 value,
-    _In_ DWORD valueSize)
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode)
 {
     FILE* pFile = NULL;
     FILE* pFilteredFile = NULL;  // Used by `filter` command. It contains filtered addresses
     PVOID address = NULL;
     DWORD64 processValue = 0;
+    BOOL result = FALSE;
 
-    if (fopen_s(&pFilteredFile, FILTERED_FILE_NAME, "w")) {
+    if (fopen_s(&pFilteredFile, FILTERED_FILE_NAME, "w"))
         throw std::runtime_error("Failed to open output pFile");
-    }
-    if (fopen_s(&pFile, FILE_NAME, "r")) {
+    if (fopen_s(&pFile, FILE_NAME, "r"))
         throw std::runtime_error("Failed to open filter pFile");
-    }
 
     // Read addresses from `pFile`, compare their values to `value`. Matching one write to
     // `pFilteredFile`.
     while (fscanf_s(pFile, "%p", &address) != EOF) {
         LOG(INFO) << "address: " << address;
-        if (ReadProcessMemory(
+        if (isDriverMode == FALSE) {
+            result = ReadProcessMemory(
                 process,
                 address,
                 &processValue,
                 valueSize,
-                NULL)) {
+                NULL);
+        }
+        else {
+            result = ReadProcessMemoryDrivered(
+                process,
+                address,
+                &processValue,
+                valueSize,
+                NULL);
+        }
+
+        if (result) {
             LOG(INFO) << "processValue: " << processValue;
             if (memcmp(&processValue, &value, valueSize) == 0) {
                 fprintf(pFilteredFile, "%p\n", address);
@@ -346,67 +471,52 @@ void FilterProcessAddresses(
                 Value to write.
             _In_ DWORD valueSize)
                 Value size.
+            _In_ BOOL isDriverMode
+                Specify if function should use kernel driver.
 
   Returns:  void
 -----------------------------------------------------------------F-F*/
 void WriteProcessAddresses(
     _In_ HANDLE process,
     _In_ DWORD64 value,
-    _In_ DWORD valueSize)
+    _In_ DWORD valueSize,
+    _In_ BOOL isDriverMode)
 {
     FILE* pFile = NULL;
     PVOID address = NULL;
+    BOOL result = FALSE;
 
     if (fopen_s(&pFile, FILE_NAME, "r")) {
         throw std::runtime_error("Failed to open filter pFile");
     }
 
     while (fscanf_s(pFile, "%p", &address) != EOF) {
-        if (WriteProcessMemory(
+        if (isDriverMode) {
+            result = WriteProcessMemory(
                 process,
                 address,
                 &value,
                 valueSize,
-                NULL)) {
+                NULL);
+        }
+        else {
+            result = WriteProcessMemoryDrivered(
+                process,
+                address,
+                &value,
+                valueSize,
+                NULL);
+        }
+        if (result) {
             LOG(INFO) << "address 0x" << std::hex << address << " is written";
         }
         else {
-            std::runtime_error("Failed to write to specified process");
+            throw std::runtime_error("Failed to write to specified process");
         }
     }
 
     if (pFile)
         fclose(pFile);
-}
-
-DWORD64 ParseValue(
-    _In_ std::string valueStr,
-    _In_ std::string valueType,
-    _In_ PDWORD pValueSize)
-{
-    DWORD64 value = 0;
-    if (valueType == "int32") {
-        value = (DWORD64)std::stol(valueStr);
-        *pValueSize = sizeof(INT32);
-    }
-    else if (valueType == "int64") {
-        value = (DWORD64)std::stoll(valueStr);
-        *pValueSize = sizeof(INT64);
-    }
-    else if (valueType == "float") {
-        FLOAT valueFloat = std::stof(valueStr);
-        memcpy(&value, &valueFloat, sizeof(FLOAT));
-        *pValueSize = sizeof(FLOAT);
-    }
-    else if (valueType == "double") {
-        DOUBLE valueDouble = std::stod(valueStr);
-        memcpy(&value, &valueDouble, sizeof(DOUBLE));
-        *pValueSize = sizeof(DOUBLE);
-    }
-    else {
-        throw TCLAP::ArgParseException("--value-type");
-    }
-    return value;
 }
 
 void OutputFile()
